2282b1feaca534f2ece4e57646a111042f777343
Theesud <Theesud.Chuchaiwattanasak@gmail.com>
2026-08-17 04:31:12 +0700

    increase delay for the last update

--- app/page.js
+++ app/page.js
@@ -349,7 +349,7 @@ export default function Portfolio() {
       </section>
 
 
-      <FadeIn delay={200}>
+      <FadeIn delay={400}>
     {/* คะแนน Toeic */}   
      <section className="py-12 bg-white">
       <div className="container mx-auto px-4 max-w-2xl">
@@ -390,7 +390,7 @@ export default function Portfolio() {
 
 
       {/* Contact */}
-      <FadeIn delay={400}>
+      <FadeIn delay={600}>
       <section className="py-12 bg-gray-50">
       <div className="container mx-auto px-4 text-center">
         <h2 className="text-3xl font-bold text-gray-800 mb-8">Contact Me</h2>
@@ -420,7 +420,7 @@ export default function Portfolio() {
 </FadeIn>
 
       {/* SECTION 4: FOOTER */}
-            <FadeIn delay={600}>
+            <FadeIn delay={800}>
       <footer className="bg-black text-white py-10 text-center">
         <p className="text-sm text-gray-400">
           Portfolio Website by Theesud Chuchaiwattanasak (Ray).<br />
